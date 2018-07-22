b6fd3dfe0349e6256d99a2e5e16b9a9ce0016f53
merkerxu <merkerxu@163.com>
2018-07-23 00:48:26 +0800

    a 'fine' ex1.4

---
+++ ch1/ex1.4.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "io/ioutil"
+    "strings"
+)
+
+func main() {
+    counts := make(map[string]int)
+    filenames := make(map[string]string)
+    for _, filename := range os.Args[1:] {
+        data, err := ioutil.ReadFile(filename)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "ex1.4: %v\n", err)
+            continue
+        }
+        for _, line := range strings.Split(string(data), "\n") {
+            counts[line]++
+            filenames[line] += " " + filename
+        }
+    }
+
+    for line, n := range counts {
+        if n > 1 {
+            fmt.Printf("%d\t%s\t%s\n", n, line, filenames[line])
+        }
+    }
+}
