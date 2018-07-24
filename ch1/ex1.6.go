2f70748072bb5c66f375495d4b62d32f06a31d24
merkerxu <merkerxu@163.com>
2018-07-25 00:33:42 +0800

    fix bug: blackColor & color pick

--- ch1/ex1.6.go
+++ ch1/ex1.6.go
@@ -12,7 +12,7 @@ import (
 
 var palette = []color.Color{color.White, color.RGBA{255,0,0,255},
                     color.RGBA{0,255,0,255}, color.RGBA{0,0,255,255},
-                    color.RGBA{255,255,255,255}}
+                    color.RGBA{0,0,0,255}}
 
 const (
     whiteIndex = 0
@@ -42,10 +42,10 @@ func lissajous(out io.Writer) {
     for i:=0; i<nframes; i++ {
         rect := image.Rect(0,0,2*size+1,2*size+1)
         img  := image.NewPaletted(rect, palette)
+        colorIndex := getColorIndex()
         for t:= 0.0; t<cycles*2*math.Pi; t+=res {
             x := math.Sin(t)
             y := math.Sin(t*freq + phase)
-            colorIndex := getColorIndex()
             img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
                 colorIndex)
         }
