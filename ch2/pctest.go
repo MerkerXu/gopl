7483b210be0c60440508ddc5d1d7e30cba64ae04
merkerxu <merkerxu@163.com>
2018-07-31 19:14:41 +0800

    add args

--- ch2/pctest.go
+++ ch2/pctest.go
@@ -8,6 +8,10 @@ import (
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("usage: %s <integer>\n", os.Args[0])
+		os.Exit(1)
+	}
 	input, err := strconv.ParseUint(os.Args[1], 10, 64)
 	if err != nil {
 		fmt.Printf("%v\n", err)
