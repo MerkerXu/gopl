ddb02870469aefcaafcf426c37970ed715a5480c
merkerxu <merkerxu@163.com>
2018-12-10 15:26:37 +0800

    add fmt

--- ch3/signed.go
+++ ch3/signed.go
@@ -7,4 +7,9 @@ func main() {
     for i:= len(medals)-1; i>=0; i-- {
         fmt.Println(medals[i])
     }
+
+    o := 0666
+    fmt.Printf("%d %[1]o %#[1]o\n", o)
+    x := int64(0xdeadbeef)
+    fmt.Printf("%d %[1]x %#[1]x %#[1]X\n", x)
 }
