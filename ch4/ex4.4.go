bef2b16f5894c0b6bf4f7545c5720bee7e41930c
merkerxu <merkerxu@163.com>
2019-01-09 23:16:14 +0800

    rotate one pass: 2 cases gcd or not

--- ch4/ex4.4.go
+++ ch4/ex4.4.go
@@ -3,11 +3,27 @@ package main
 import "fmt"
 
 func rotate(a []int, k int) {
+	length := len(a)
+	k = k % length
+	count := 0
+	for startPos := 0; count < length; startPos++ {
+		nextPos := (startPos + (length - k)) % length
+		nextVal := a[nextPos]
+		a[nextPos] = a[startPos]
+		currPos := nextPos
+		count++
+		for startPos != currPos {
+			nextPos := (currPos + (length - k)) % length
+			nextVal, a[nextPos] = a[nextPos], nextVal
+			count++
+			currPos = nextPos
+		}
+	}
 }
 
 func main() {
-    a := [...]int{1,2,3,4,5,6}
-    fmt.Println(a)
-    rotate(a[:], 2)
-    fmt.Println(a)
+	a := [...]int{1, 2, 3, 4, 5, 6, 8}
+	fmt.Println(a)
+	rotate(a[:], 4)
+	fmt.Println(a)
 }
